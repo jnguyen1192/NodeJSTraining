22cc12a9eb069e15e34293f861752804ff5c7072
jnguyen1192 <johdu13@hotmail.fr>
2020-03-25 07:46:18 +0100

    #1 part 3 done

--- Part3 Manage file system module/w3schoolTrainingPart3.js
+++ Part3 Manage file system module/w3schoolTrainingPart3.js
@@ -2,6 +2,30 @@
 var http = require('http'); // http module
 var fs = require('fs'); // fs module
 
+fs.appendFile('mynewfile1.txt', 'Hello Header Data Scientist!', function (err) { // create or update file and write on it
+  if (err) throw err;
+  console.log('mynewfile1.txt Saved or updated!');
+});
+
+fs.open('mynewfile2.txt', 'w', function (err) { // open file to create it
+  if (err) throw err;
+  console.log('mynewfile2.txt Saved!');
+});
+
+fs.writeFile('mynewfile3.txt', 'Hello Data Scientist!', function (err) { // open to create it or update file
+  if (err) throw err;
+  console.log('mynewfile3.txt Saved or replaced!');
+});
+
+fs.unlink('mynewfile2.txt', function (err) { // delete file
+  if (err) throw err;
+  console.log('mynewfile2.txt File deleted!');
+});
+
+fs.rename('mynewfile1.txt', 'myrenamedfile.txt', function (err) { // rename file
+  if (err) throw err;
+  console.log('File Renamed!');
+});
 
 http.createServer(function (req, res) {
   fs.readFile('demofile1.html', function(err, data) {
