770ebab43126fb4aa3653226a5c375d0dede50b1
jnguyen1192 <johdu13@hotmail.fr>
2020-03-25 07:22:31 +0100

    #1 part 2 really done

--- Part2/w3schoolTrainingPart2.js
+++ Part2/w3schoolTrainingPart2.js
@@ -1,12 +1,15 @@
 // create the server
-var http = require('http');
+var http = require('http'); // http module
+var url = require('url'); // url module
 
 var dt = require('./myfirstmodule');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write("The date and time are currently: " + dt.myDateTime());
-  res.end();
+  res.writeHead(200, {'Content-Type': 'text/plain'}); // http header
+  var q = url.parse(req.url, true).query; // split query
+  var text = q.year + " " + q.month;
+  res.write("The date and time are currently: " + dt.myDateTime() + "\nUrl: " + req.url + "\n");
+  res.end("year and month: "+ text);
 }).listen(8080);
 
 // Print on the console
